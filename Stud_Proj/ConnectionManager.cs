91561d51aa70b7669ce9556ea8904e7088387aa8
andreicalugaru <andreix34@gmail.com>
2017-03-15 10:13:22 -0700

    conexiune reusita

---
+++ Stud_Proj/ConnectionManager.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stud_Proj
+{
+    class ConnectionManager
+    {
+        string connectionSettings;
+
+        public ConnectionManager()
+        {
+            connectionSettings = @"server=127.0.0.1;port=3306;userid=root;password=root;database=studentsdb";
+        }
+
+        public String getName(int id)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionSettings))
+            {
+                connection.Open();
+                string statement = "SELECT Name FROM student WHERE id=1";
+                MySqlCommand cmd = new MySqlCommand(statement, conection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while(reader.Read())
+                    {
+                        Console.WriteLine(reader.getString);
+                    }
+                }
+            }
+        }
+    }
+
+
+}
---
+++ Stud_Proj/Stud_Proj/ConnectionManager.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stud_Proj
+{
+    class ConnectionManager
+    {
+        string connectionSettings;
+
+        public ConnectionManager()
+        {
+            connectionSettings = @"server=127.0.0.1;port=3306;userid=root;password=;database=studentsdb";
+        }
+
+        public void GetName(int id)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionSettings))
+            {
+                connection.Open();
+                string statement = "SELECT Nume FROM student WHERE idStudent=1";
+                MySqlCommand cmd = new MySqlCommand(statement, connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                //if (reader.HasRows)
+                //{
+                //    while(reader.Read())
+                //   {
+                //       string nume = reader.GetString("Nume");
+                //       Console.WriteLine(nume);
+                //   }
+                // }
+                reader.Read();
+                Console.WriteLine(reader.GetString("Nume"));
+
+            }
+        }
+    }
+
+
+}
